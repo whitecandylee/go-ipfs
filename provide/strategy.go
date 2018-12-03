217518c97c535c96a17a6a5d4189ba78ac85415c
Michael Avila <davidmichaelavila@gmail.com>
2018-12-03 08:06:27 -0800

    Fix bad gx merkledag version

--- provide/strategy.go
+++ provide/strategy.go
@@ -3,7 +3,7 @@ package provide
 import (
 	"context"
 	"gx/ipfs/QmR8BauakNcBa3RbE4nbQu76PDiJgoQgz8AJdhJuiU4TAw/go-cid"
-	"gx/ipfs/QmcGt25mrjuB2kKW2zhPbXVZNHc4yoTDQ65NA8m6auP2f1/go-merkledag"
+	"gx/ipfs/QmdURv6Sbob8TVW2tFFve9vcEWrSUgwPqeqnXyvYhLrkyd/go-merkledag"
 	ipld "gx/ipfs/QmcKKBwfz6FyQdHR2jsXrrF6XeSBXYL86anmWNewpFpoF5/go-ipld-format"
 )
 
